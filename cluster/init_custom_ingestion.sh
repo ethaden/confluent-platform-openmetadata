ae430835823121844a746105ba0162b0d832dfe5
Eike Thaden <eike.thaden@gmail.com>
2024-04-12 20:52:39 +0200

    Added Mariadb service. Need to fix MariaDB ingestion

--- cluster/init_custom_ingestion.sh
+++ cluster/init_custom_ingestion.sh
@@ -2,7 +2,9 @@
 
 # JWTs have limited validity. We need to grab a newly created one
 
-JWT_TOKEN=$(grep -oP '(?<=jwtToken: \")[^"]+' /opt/airflow/dags/airflow_docker_operator.py)
+if [ -z ${JWT_TOKEN} ]; then
+  JWT_TOKEN=$(grep -oP '(?<=jwtToken: \")[^"]+' /opt/airflow/dags/airflow_docker_operator.py)
+fi
 
 echo "Creating topics"
 kafka-topics --bootstrap-server broker:9092 --create --topic topic-1 --partitions 1
@@ -95,6 +97,83 @@ if [ -n ${SERVICE_ID} ]; then
         http://openmetadata_server:8585/api/v1/services/ingestionPipelines | grep -oP '(?<=^{"id":")[^"]+')
         #
         echo "Deploying ingestion for Kafka service..."
+        curl --request POST --header "Content-Type: application/json" -H "Authorization: Bearer ${JWT_TOKEN}" http://openmetadata_server:8585/api/v1/services/ingestionPipelines/deploy/${INGESTION_ID}
+fi
+echo "Setting up MariaDB service"
+export SERVICE_ID=$(curl --request POST --header "Content-Type: application/json" -H "Authorization: Bearer ${JWT_TOKEN}" --data \
+"{\"name\":\"mariadb\",
+  \"serviceType\":\"MariaDB\",
+  \"description\":\"\",
+  \"connection\":{\"config\":
+    {\"type\":\"MariaDB\",
+      \"scheme\":\"mysql+pymysql\",
+      \"username\":\"${MARIADB_OPENMETADATA_USER}\",
+      \"password\":\"${MARIADB_OPENMETADATA_PASSWORD}\",
+      \"hostPort\":\"mariadb:3306\",
+      \"databaseName\":\"demo\",
+      \"supportsMetadataExtraction\":true,
+      \"supportsDBTExtraction\":true,
+      \"supportsProfiler\":true,
+      \"supportsQueryComment\":true
+    }}
+}" \
+http://openmetadata_server:8585/api/v1/services/databaseServices | grep -oP '(?<="id":")[^"]+')
+if [ -n ${SERVICE_ID} ]; then
+    echo "Created service Kafka with ID ${SERVICE_ID}"
+    echo "Creating ingestion pipeline for kafka..."
+    export INGESTION_ID=$(curl --request POST --header "Content-Type: application/json" -H "Authorization: Bearer ${JWT_TOKEN}" --data \
+        "{\"name\":\"mariadb_metadata_wUMTfesN\",
+          \"displayName\":\"mariadb_metadata_wUMTfesN\",
+          \"pipelineType\":\"metadata\",
+          \"sourceConfig\":
+          {\"type\":\"DatabaseMetadata\",
+            \"includeTags\":true,
+            \"includeViews\":true,
+            \"includeTables\":true,
+            \"markDeletedTables\":true,
+            \"tableFilterPattern\":{
+                \"excludes\":[],
+                \"includes\":[]
+            },
+            \"useFqnForFiltering\":false,
+            \"schemaFilterPattern\":{
+                \"excludes\":[],
+                \"includes\":[]
+            },
+            \"markAllDeletedTables\":false,
+            \"databaseFilterPattern\":{
+                \"excludes\":[],
+                \"includes\":[]
+            },
+            \"viewParsingTimeoutLimit\":300
+          },
+          \"airflowConfig\":
+          {\"pausePipeline\":false,
+           \"concurrency\":1,
+           \"startDate\":1712275200000,
+           \"pipelineTimezone\":\"UTC\",
+           \"retries\":3,
+           \"retryDelay\":300,
+           \"pipelineCatchup\":false,
+           \"scheduleInterval\":\"*/5 * * * *\",
+           \"maxActiveRuns\":1,
+           \"workflowDefaultView\":\"tree\",
+           \"workflowDefaultViewOrientation\":\"LR\"
+          },
+          \"service\":
+          {
+           \"id\":\"${SERVICE_ID}\",\"type\":
+           \"databaseService\",
+           \"name\":\"mariadb\",
+           \"fullyQualifiedName\":\"mariadb\",
+           \"description\":\"\",
+           \"deleted\":false,
+           \"href\":\"http://localhost:8585/api/v1/services/databaseServices/${SERVICE_ID}\"
+          } 
+        }" \
+        http://openmetadata_server:8585/api/v1/services/ingestionPipelines | grep -oP '(?<=^{"id":")[^"]+')
+        #
+        echo "Deploying ingestion for Mariadb service..."
         curl --request POST --header "Content-Type: application/json" -H "Authorization: Bearer ${JWT_TOKEN}" http://openmetadata_server:8585/api/v1/services/ingestionPipelines/deploy/${INGESTION_ID}
 fi
 
